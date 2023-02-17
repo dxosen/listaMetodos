91c9c732f3d200cae40e160f20383fa76185033a
Gabriel Dario <90010767+dxosen@users.noreply.github.com>
2023-02-17 18:31:07 -0300

    Update ex6.java

--- ex6.java
+++ ex6.java
@@ -27,8 +27,8 @@ public class ex6 {
 
 	public static boolean verificardorMultipNunConsec(int valorBase) {
 
-		for (int i = 0; i < valorBase; i++) {
-			for (int j = 0; j < valorBase; j++) {
+		for (int i = 2; i < (valorBase+2); i++) {
+			for (int j = 1; j <= valorBase; j++) {
 				if (valorBase == (Math.pow(j, i))) {
 					System.out.println(j + "^" + i + " = " + (Math.pow(j, i)));
 					return true;
